26e671ffc577fa060dfda03162a56879ef6b6286
David Vainer <david@korfonus.com>
2024-12-07 16:44:52 +0200

    init(client-project) - create client side project and implemented most of the logic

---
+++ ChatApp.Client/chat-app/src/components/Layout.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Box, Button, Typography, AppBar, Toolbar } from "@mui/material";
+
+const Layout = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const showLogoutButton = location.pathname !== "/login";
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+
+        navigate("/login");
+    };
+
+    return (
+        <Box>
+            <AppBar position="static" color="primary">
+                <Toolbar sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Typography variant="h4">Chat App</Typography>
+
+                    {showLogoutButton && (
+                        <Button variant="contained" color="secondary" onClick={handleLogout}>
+                            Logout
+                        </Button>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <Box flex="1" p={5} margin={"auto"}>
+                <Outlet />
+            </Box>
+        </Box>
+    );
+};
+
+export default Layout;
