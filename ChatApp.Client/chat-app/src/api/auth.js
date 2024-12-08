ee247485fddc1cb7ca9453b6de807419643106fd
David Vainer <david@korfonus.com>
2024-12-08 20:14:34 +0200

    fix(disable-http) - fix container startup fail by using http instead of https

--- ChatApp.Client/chat-app/src/api/auth.js
+++ ChatApp.Client/chat-app/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://localhost:8081/api/auth";
+const API_BASE_URL = "http://localhost:8080/api/auth";
 
 export const loginUser = async (email, password) => {
     const response = await axios.post(`${API_BASE_URL}/login`, { email, password});
